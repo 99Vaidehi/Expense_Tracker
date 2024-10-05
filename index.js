85d289dc830316e4c0ca15ef052815bc6e35c20e
Vaidehi Gupta <154899023+99Vaidehi@users.noreply.github.com>
2024-10-05 22:02:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+const { Router } = require("express");
+const expenseRouter = require("./expenseRoutes");
+const userRouter = require("./userRoutes");
+const router = Router();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+router.use("/user", userRouter);
+router.use("/expense", expenseRouter);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+module.exports = router;
